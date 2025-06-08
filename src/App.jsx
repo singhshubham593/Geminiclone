faa424b6b372cf89bf3ba80ac919fcb806c3f9c1
singhshubham593 <92996831+singhshubham593@users.noreply.github.com>
2025-06-08 14:33:32 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Answers from './components/Answers.jsx'
 
 function App() {
    const [question,setQuestion] = useState('');
-   const [result,setResult] = useState('');
+   const [result,setResult] = useState([]);
 
   const payload ={
     "contents": [{
@@ -27,10 +27,10 @@ function App() {
     let dataString = response.candidates[0].content.parts[0].text;
     dataString = dataString.split("* ");
     dataString =dataString.map((item)=>item.trim())
-    console.log(dataString);
-    setResult(dataString);
+    //console.log(dataString);
+    setResult([...result,{type:'q',text:question},{type:'a',text:dataString}]);
    }
-
+   console.log(result);
 
   return (
      <div className="grid grid-cols-5 h-screen text-center">
@@ -40,12 +40,14 @@ function App() {
             <div className='text-zinc-300'>
               <ul>
                 {
-                  result && result.map((item,index) =>(
-                    <li className='text-left p-10'><Answers ans={item} totalResult={result.length} index={index}/></li>
-                  ))
+                  result.map((item,index) =>(
+                    item.type === 'q' ? 
+                    <li key={index+Math.random()} className='text-left p-1'><Answers ans={item.text} totalResult={1} index={index}/></li>
+                    :item.text.map((ansItem,ansIndex) =>(
+                    <li key={ansIndex+Math.random()} className='text-left p-1'><Answers ans={ansItem} totalResult={item.length} index={ansIndex}/></li>
+                  ))))
                 }
-              </ul>
-               
+              </ul> 
             </div>
           </div>
           <div className="bg-zinc-800 w-1/2 p-1 text-white m-auto mt-4 rounded-4xl border border-zinc-700 flex h-16">
